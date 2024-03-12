207615ab89ad832e9041af7b3364ceeba0c93620
qin zhen <qtoq126@163.com>
2024-03-12 18:45:29 +0800

    Initial Commit

---
+++ SspaiDemo/SspaiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SspaiDemoApp.swift
+//  SspaiDemo
+//
+//  Created by Zhen Qin on 2024/3/12.
+//
+
+import SwiftUI
+
+@main
+struct SspaiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
